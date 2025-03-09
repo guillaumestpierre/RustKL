ed068c2ac071132f0af40af09655bc67a81760a3
guillaumestpierre <Guillaumestpierre4212003@gmail.com>
2025-03-09 12:51:57 -0400

    fix: everything works for now

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::path::Path;
 use std::io;
 
 fn main() -> std::io::Result<()>{
-    // Récupérer le path de l'image
+    // Get image path
     println!("Enter the path to the original image.");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read from stdin");
@@ -21,17 +21,16 @@ fn main() -> std::io::Result<()>{
         io::stdin().read_line(&mut image_path_str).expect("Failed to read from stdin");
         image_path_str = image_path_str.trim().to_string();
     }
-
+    
     let image_path = Path::new(&image_path_str);
 
-    // Charger l'image
-    let img = image::open(image_path).expect("Impossible de charger l'image");
+    // Loading image
+    let img = image::open(image_path).expect("Could not load the image !");
     let (width, height) = img.dimensions();
     let nb_pixels = (width * height) as f64;
 
-    // Stocker les valeurs des pixels sous forme de matrices
+    // Stocking pixel values in matrix
     let mut image_matrix = DMatrix::<f64>::zeros((height * width) as usize, 3);
-
     for (x, y, pixel) in img.pixels() {
         let index = (y * width + x) as usize;
         let rgb = pixel.to_rgb();
@@ -40,145 +39,90 @@ fn main() -> std::io::Result<()>{
         image_matrix[(index, 2)] = rgb[2] as f64;
     }
 
-    // Calcul des moyennes
+    // Mean calculation
     let moy_r = image_matrix.column(0).sum() / nb_pixels;
     let moy_g = image_matrix.column(1).sum() / nb_pixels;
     let moy_b = image_matrix.column(2).sum() / nb_pixels;
+    println!("Mean R: {:.2}, G: {:.2}, B: {:.2}", moy_r, moy_g, moy_b);
 
-    println!("Moyenne R: {:.2}, G: {:.2}, B: {:.2}", moy_r, moy_g, moy_b);
+    // Keeping a copy of the image
+    let image_matrix_original = image_matrix.clone();
 
-    // Centrage des données
+    // Data centering
     for i in 0..image_matrix.nrows() {
         image_matrix[(i, 0)] -= moy_r;
         image_matrix[(i, 1)] -= moy_g;
         image_matrix[(i, 2)] -= moy_b;
     }
 
-    // Calcul de la matrice de covariance : (X^T * X) / N
+    // Calculating cov : (X^T * X) / N
     let cov_rgb = (image_matrix.transpose() * &image_matrix) / nb_pixels;
-    println!("Matrice de covariance:\n{}", cov_rgb);
+    println!("Cov matrix:\n{}", cov_rgb);
 
-    // Calcul des valeurs propres et vecteurs propres avec nalgebra
+    // Eig values and vectors
     let eig = cov_rgb.symmetric_eigen();
-    println!("Valeurs propres:\n{}", eig.eigenvalues);
-    println!("Vecteurs propres:\n{}", eig.eigenvectors);
+    println!("Eigenvalues:\n{}", eig.eigenvalues);
+    println!("Eigenvectors:\n{}", eig.eigenvectors);
 
+    // Deleting components
+    let mut diag_sans_axe0 = DMatrix::<f64>::identity(3, 3);
+    diag_sans_axe0[(0, 0)] = 0.0;
 
-    //Une composante sur trois retirée
-    let trans_eig_vec = eig.eigenvectors.transpose();
+    let mut diag_sans_axe1 = DMatrix::<f64>::identity(3, 3);
+    diag_sans_axe1[(1, 1)] = 0.0;
 
-    let mut eig_vec_sans_axe0 = trans_eig_vec.clone();
-    eig_vec_sans_axe0.fill_row(0, 0.0);
+    let mut diag_sans_axe2 = DMatrix::<f64>::identity(3, 3);
+    diag_sans_axe2[(2, 2)] = 0.0;
 
-    let mut eig_vec_sans_axe1 = trans_eig_vec.clone();
-    eig_vec_sans_axe1.fill_row(1, 0.0);
+    // Projection matrix
+    let proj_sans_axe0 = &eig.eigenvectors * &diag_sans_axe0 * &eig.eigenvectors.transpose();
+    let proj_sans_axe1 = &eig.eigenvectors * &diag_sans_axe1 * &eig.eigenvectors.transpose();
+    let proj_sans_axe2 = &eig.eigenvectors * &diag_sans_axe2 * &eig.eigenvectors.transpose();
 
-    let mut eig_vec_sans_axe2 = trans_eig_vec.clone();
-    eig_vec_sans_axe2.fill_row(2, 0.0);
-
-    let mut image_KL_sans_axe0 = image_matrix.clone();
-    let mut image_KL_sans_axe1 = image_matrix.clone();
-    let mut image_KL_sans_axe2 = image_matrix.clone();
+    // Stocking the results in matrix
+    let mut image_RGB_sans_axe0 = DMatrix::<f64>::zeros(image_matrix.nrows(), 3);
+    let mut image_RGB_sans_axe1 = DMatrix::<f64>::zeros(image_matrix.nrows(), 3);
+    let mut image_RGB_sans_axe2 = DMatrix::<f64>::zeros(image_matrix.nrows(), 3);
 
     let vec_moy = DVector::<f64>::from_vec(vec![moy_r, moy_g, moy_b]);
 
-    for i in 0..image_matrix.nrows() {
-        // Création du vecteur pour chaque pixel
-        let vec_temp = DVector::<f64>::from_vec(vec![
-            image_matrix[(i, 0)],
-            image_matrix[(i, 1)],
-            image_matrix[(i, 2)],
+    for i in 0..image_matrix_original.nrows() {
+        // Creating a vector for each pixel
+        let vec_original = DVector::<f64>::from_vec(vec![
+            image_matrix_original[(i, 0)],
+            image_matrix_original[(i, 1)],
+            image_matrix_original[(i, 2)],
         ]);
-
-        // Calcul de la soustraction du vecteur moyen
-        let temp_minus_moy = &vec_temp - &vec_moy;
-
-        // Calcul des nouvelles valeurs pour chaque image transformée
-        let transformed_axe0 = &eig_vec_sans_axe0 * &temp_minus_moy;
-        let transformed_axe1 = &eig_vec_sans_axe1 * &temp_minus_moy;
-        let transformed_axe2 = &eig_vec_sans_axe2 * &temp_minus_moy;
-
-        // Stockage dans les matrices de sortie
-        image_KL_sans_axe0[(i, 0)] = transformed_axe0[0];
-        image_KL_sans_axe0[(i, 1)] = transformed_axe0[1];
-        image_KL_sans_axe0[(i, 2)] = transformed_axe0[2];
-
-        image_KL_sans_axe1[(i, 0)] = transformed_axe1[0];
-        image_KL_sans_axe1[(i, 1)] = transformed_axe1[1];
-        image_KL_sans_axe1[(i, 2)] = transformed_axe1[2];
-
-        image_KL_sans_axe2[(i, 0)] = transformed_axe2[0];
-        image_KL_sans_axe2[(i, 1)] = transformed_axe2[1];
-        image_KL_sans_axe2[(i, 2)] = transformed_axe2[2];
-    }      
-
-    //Reconstruction des images
-    let inv_eigvec_sans_axe0 = eig_vec_sans_axe0.clone().pseudo_inverse(1e-10);
-    let inv_eigvec_sans_axe1 = eig_vec_sans_axe1.clone().pseudo_inverse(1e-10);
-    let inv_eigvec_sans_axe2 = eig_vec_sans_axe2.clone().pseudo_inverse(1e-10);
-
-    if !inv_eigvec_sans_axe0.is_ok() || !inv_eigvec_sans_axe1.is_ok() || !inv_eigvec_sans_axe2.is_ok() {
-        panic!("Une des matrices modifiées n'est pas inversible !");
+        
+        // Centering the data
+        let vec_centered = &vec_original - &vec_moy;
+        
+        // Apply projection
+        let proj_vec0 = &proj_sans_axe0 * &vec_centered;
+        let proj_vec1 = &proj_sans_axe1 * &vec_centered;
+        let proj_vec2 = &proj_sans_axe2 * &vec_centered;
+        
+        // Reconstruct
+        let reconstructed0 = &proj_vec0 + &vec_moy;
+        let reconstructed1 = &proj_vec1 + &vec_moy;
+        let reconstructed2 = &proj_vec2 + &vec_moy;
+        
+        // Stocking the values with clamping
+        image_RGB_sans_axe0[(i, 0)] = reconstructed0[0].clamp(0.0, 255.0);
+        image_RGB_sans_axe0[(i, 1)] = reconstructed0[1].clamp(0.0, 255.0);
+        image_RGB_sans_axe0[(i, 2)] = reconstructed0[2].clamp(0.0, 255.0);
+        
+        image_RGB_sans_axe1[(i, 0)] = reconstructed1[0].clamp(0.0, 255.0);
+        image_RGB_sans_axe1[(i, 1)] = reconstructed1[1].clamp(0.0, 255.0);
+        image_RGB_sans_axe1[(i, 2)] = reconstructed1[2].clamp(0.0, 255.0);
+        
+        image_RGB_sans_axe2[(i, 0)] = reconstructed2[0].clamp(0.0, 255.0);
+        image_RGB_sans_axe2[(i, 1)] = reconstructed2[1].clamp(0.0, 255.0);
+        image_RGB_sans_axe2[(i, 2)] = reconstructed2[2].clamp(0.0, 255.0);
     }
-
-    let inv_eigvec_sans_axe0 = inv_eigvec_sans_axe0.unwrap();
-    let inv_eigvec_sans_axe1 = inv_eigvec_sans_axe1.unwrap();
-    let inv_eigvec_sans_axe2 = inv_eigvec_sans_axe2.unwrap();
-
-    let mut image_RGB_sans_axe0 = image_matrix.clone();
-    let mut image_RGB_sans_axe1 = image_matrix.clone();
-    let mut image_RGB_sans_axe2 = image_matrix.clone();
-
-    for i in 0..image_matrix.nrows() {
-        // Création du vecteur pour chaque pixel
-        let vec_temp_0 = DVector::<f64>::from_vec(vec![
-            image_KL_sans_axe0[(i, 0)],
-            image_KL_sans_axe0[(i, 1)],
-            image_KL_sans_axe0[(i, 2)],
-        ]);
-
-        let vec_temp_1 = DVector::<f64>::from_vec(vec![
-            image_KL_sans_axe1[(i, 0)],
-            image_KL_sans_axe1[(i, 1)],
-            image_KL_sans_axe1[(i, 2)],
-        ]);
-
-        let vec_temp_2 = DVector::<f64>::from_vec(vec![
-            image_KL_sans_axe2[(i, 0)],
-            image_KL_sans_axe2[(i, 1)],
-            image_KL_sans_axe2[(i, 2)],
-        ]);
-
-        let reconstructed_axe0 = &inv_eigvec_sans_axe0 * vec_temp_0 + &vec_moy;
-        let reconstructed_axe1 = &inv_eigvec_sans_axe1 * vec_temp_1 + &vec_moy;
-        let reconstructed_axe2 = &inv_eigvec_sans_axe2 * vec_temp_2 + &vec_moy;
-
-        // Stockage dans les matrices RGB reconstruites
-        image_RGB_sans_axe0[(i, 0)] = reconstructed_axe0[0];
-        image_RGB_sans_axe0[(i, 0)] = image_RGB_sans_axe0[(i, 0)].clamp(0.0, 255.0);
-        image_RGB_sans_axe0[(i, 1)] = reconstructed_axe0[1];
-        image_RGB_sans_axe0[(i, 1)] = image_RGB_sans_axe0[(i, 1)].clamp(0.0, 255.0);
-        image_RGB_sans_axe0[(i, 2)] = reconstructed_axe0[2];
-        image_RGB_sans_axe0[(i, 2)] = image_RGB_sans_axe0[(i, 2)].clamp(0.0, 255.0);
-
-        image_RGB_sans_axe1[(i, 0)] = reconstructed_axe1[0];
-        image_RGB_sans_axe1[(i, 0)] = image_RGB_sans_axe1[(i, 0)].clamp(0.0, 255.0);
-        image_RGB_sans_axe1[(i, 1)] = reconstructed_axe1[1];
-        image_RGB_sans_axe1[(i, 1)] = image_RGB_sans_axe1[(i, 1)].clamp(0.0, 255.0);
-        image_RGB_sans_axe1[(i, 2)] = reconstructed_axe1[2];
-        image_RGB_sans_axe1[(i, 2)] = image_RGB_sans_axe1[(i, 2)].clamp(0.0, 255.0);
-
-        image_RGB_sans_axe2[(i, 0)] = reconstructed_axe2[0];
-        image_RGB_sans_axe2[(i, 0)] = image_RGB_sans_axe2[(i, 0)].clamp(0.0, 255.0);
-        image_RGB_sans_axe2[(i, 1)] = reconstructed_axe2[1];
-        image_RGB_sans_axe2[(i, 1)] = image_RGB_sans_axe2[(i, 1)].clamp(0.0, 255.0);
-        image_RGB_sans_axe2[(i, 2)] = reconstructed_axe2[2];
-        image_RGB_sans_axe2[(i, 2)] = image_RGB_sans_axe2[(i, 2)].clamp(0.0, 255.0);
-    } 
 
     //Saving reconstructed images
     let (mut img_reconstructed0, mut img_reconstructed1, mut img_reconstructed2) = (image::RgbImage::new(width, height), image::RgbImage::new(width, height),image::RgbImage::new(width, height));
-
     for y in 0..height {
         for x in 0..width {
             let index = (y * width + x) as usize;
@@ -187,34 +131,29 @@ fn main() -> std::io::Result<()>{
                 image_RGB_sans_axe0[(index, 1)] as u8,
                 image_RGB_sans_axe0[(index, 2)] as u8,
             ]);
-
             let pixel1 = image::Rgb([
                 image_RGB_sans_axe1[(index, 0)] as u8,
                 image_RGB_sans_axe1[(index, 1)] as u8,
                 image_RGB_sans_axe1[(index, 2)] as u8,
             ]);
-
             let pixel2 = image::Rgb([
                 image_RGB_sans_axe2[(index, 0)] as u8,
                 image_RGB_sans_axe2[(index, 1)] as u8,
                 image_RGB_sans_axe2[(index, 2)] as u8,
             ]);
-
             img_reconstructed0.put_pixel(x, y, pixel0);
             img_reconstructed1.put_pixel(x, y, pixel1);
             img_reconstructed2.put_pixel(x, y, pixel2);
-
         }
     }
-
-    // Sauvegarde
+    // Saving
     println!("The 3 modified images are being saved to the /result folder.");
     if !Path::new("./result").exists() {
         fs::create_dir("./result")?;
     }    
-    img_reconstructed0.save("./result/reconstructed0.png").expect("Impossible de sauvegarder l'image !");
-    img_reconstructed1.save("./result/reconstructed1.png").expect("Impossible de sauvegarder l'image !");
-    img_reconstructed2.save("./result/reconstructed2.png").expect("Impossible de sauvegarder l'image !");
+    img_reconstructed0.save("./result/reconstructed0.png").expect("Could not save the image !");
+    img_reconstructed1.save("./result/reconstructed1.png").expect("Could not save the image !");
+    img_reconstructed2.save("./result/reconstructed2.png").expect("Could not save the image !");
 
     Ok(())
 
